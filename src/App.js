8da38ac3adcaf8e28024bbdb0fa50dc846ff67ee
Rafli <rahmatrafli1@gmail.com>
2022-01-10 14:49:02 +0700

    add: initialCommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Timer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: props.start,
+    };
+  }
+
+  componentDidMount() {
+    this.addInterval = setInterval(() => this.increase(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.addInterval);
+  }
+
+  increase() {
+    this.setState((state, props) => ({
+      time: parseInt(state.time) + 1,
+    }));
+  }
+
+  render() {
+    return <div>{this.state.time} Detik</div>;
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Timer start="0" />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
+
+// const name = "Yori Richard";
+
+// function greet() {
+//   return "Hello " + name;
+// }
+// const element = <h1> {greet(name)} !!</h1>;
+// ReactDOM.render(element, document.getElementById("master"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
